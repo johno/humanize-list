d63fb47383d77eeadb74d13ce3094b84306eeeab
John Otander <johnotander@gmail.com>
2015-04-07 11:30:16 -0600

    Add basic functionality

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 'use strict';
 
-module.exports = function humanizeList(options) {
-  options = options || {};
+module.exports = function humanizeList(list, options) {
+  if (!Array.isArray(list)) {
+    throw new TypeError('humanize-list expected an array');
+  }
 
-  return true;
+  options = options || {};
+  options.conjunction = options.conjunction || 'and';
+
+  var listLength = list.length;
+
+  if (options.skipConjunction) {
+    return list.join(', ');
+  }
+
+  var humanizedList = '';
+  for (var i = 0; i < listLength; i++) {
+    if (i === listLength - 1) {
+      if (options.oxfordComma) {
+        humanizedList += ',';
+      }
+
+      humanizedList += ' ' + options.conjunction + ' ';
+    } else if (i !== 0) {
+      humanizedList += ', ';
+    }
+
+    humanizedList += list[i];
+  }
+
+  return humanizedList;
 };
--- test/test.js
+++ test/test.js
@@ -5,7 +5,19 @@ var humanizeList = require('..');
 
 describe('humanize-list', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(humanizeList(), true);
+  it('should do return the correct humanized list', function() {
+    assert.equal(humanizeList(['apples', 'tomatoes', 'unicorns']), 'apples, tomatoes and unicorns');
+  });
+
+  it('should add the oxford comma when set to true', function() {
+    assert.equal(humanizeList(['apples', 'tomatoes', 'unicorns'], { oxfordComma: true }), 'apples, tomatoes, and unicorns');
+  });
+
+  it('should add a custom conjunction when specified', function() {
+    assert.equal(humanizeList(['apples', 'tomatoes', 'unicorns'], { conjunction: 'or' }), 'apples, tomatoes or unicorns');
+  });
+
+  it('should skip the conjunction when specified', function() {
+    assert.equal(humanizeList(['apples', 'tomatoes', 'unicorns'], { skipConjunction: true }), 'apples, tomatoes, unicorns');
   });
 });
